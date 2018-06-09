94e406b3b7638b4fbd1649fb6eca50849b4a4f1a
Ovidio R. Calvet <calveto@gmail.com>
2018-06-09 15:53:52 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace functionalCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var (name, age) = Who();
+            Console.WriteLine($"{name} you are {age} years old");
+        }
+        public static (string name, int age) Who() {
+            return ("Ovidio", 23);
+        }
+    }
+
+}
