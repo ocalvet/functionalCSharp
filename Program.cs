c6c65f9e12c02a7b0f8065db30fee4b482446db5
Ovidio R. Calvet <calveto@gmail.com>
2018-06-09 21:26:25 -0400

    add wallet

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace functionalCSharp
 {
@@ -7,10 +8,11 @@ namespace functionalCSharp
     {
         static void Main(string[] args)
         {
-            List<IMoney> wallet = new List<IMoney>();
-            var fiveUSDs = new Cash("$", 5m);
+            var fiveUSDs = new Cash("USD", 5m);
+            var fiveURDs = new Cash("EUR", 5m);
             var (_, tenUSDs) = fiveUSDs.Add(5m);
-            Console.WriteLine($"Todal moneys {fiveUSDs}, {tenUSDs}");
+            Wallet wallet = new Wallet(new List<IMoney>() { fiveURDs, fiveUSDs, tenUSDs });
+            wallet.Print();
         }
         public static (string name, int age) Who() {
             return ("Ovidio", 23);
@@ -27,10 +29,29 @@ namespace functionalCSharp
             this.Currency = currency;
             this.Amount = amount;
         }
+        public override string ToString() 
+        {
+            return $"{this.Amount} {this.Currency}";
+        }
+    }
+    
+    class Wallet
+    {
+        public List<IMoney> Contents { get; }
+        public Wallet(List<IMoney> contents)
+        {
+            this.Contents = contents;
+        }
     }
 
     static class ExtensionMethods
     {
+        public static void Print(this Wallet wallet)
+        {
+            wallet
+                .Contents
+                .ForEach(m => Console.WriteLine(m.ToString()));
+        }
         public static (string currency, IMoney money) Add(this IMoney money, decimal amount)
         {
             switch(money) 
