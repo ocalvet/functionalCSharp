f2156bbd75c7a63bb9fd2ed786c83a252761ab29
Ovidio R. Calvet <calveto@gmail.com>
2018-06-11 22:41:48 -0400

    add bankcard

--- Program.cs
+++ Program.cs
@@ -13,8 +13,9 @@ namespace functionalCSharp
       IEnumerable<IMoney> moneys = new List<IMoney>()
       {
         new Cash(dollar, 5m),
+        new Cash(euro, 15m),
         new Cash(euro, 5m),
-        new Cash(euro, 5m)
+        new BankCard(dollar, 100m, new DateTime(2020, 06, 30))
       };
       Wallet wallet = new Wallet(moneys);
       wallet.Print();
