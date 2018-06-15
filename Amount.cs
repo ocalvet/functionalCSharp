2c44bc5232cd71204e0bdce1a2e933b450b5e45d
Ovidio R. Calvet <calveto@gmail.com>
2018-06-14 22:57:47 -0400

    create amount class

---
+++ Amount.cs
@@ -0,0 +1,17 @@
+namespace functionalCSharp
+{
+  class Amount
+  {
+    public Currency Currency { get; }
+    public decimal Value { get; }
+    public Amount(Currency currency, decimal amount)
+    {
+      this.Currency = currency;
+      this.Value = amount;
+    }
+    public override string ToString()
+    {
+      return $"{this.Value} {this.Currency}";
+    }
+  }
+}
